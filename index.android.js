901c1253ba371ab15a5f80e28c357dd0e2c81d36
Ulisses85 <lukasz.kuligowski@gmail.com>
2017-01-16 13:26:26 +0000

    [feat-75] Initial screen and signup/MUSICIAN/BAND static

--- index.android.js
+++ index.android.js
@@ -4,50 +4,32 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   AppRegistry,
   StyleSheet,
   Text,
   View
-} from 'react-native';
+} from 'react-native'
+
+import SignupMusician from './views/signupMusician/index';
+import SignUpView from './views/signup/index';
+import InitialScreen from './views/splash/index';
 
 export default class PineappleFront extends Component {
-  render() {
+  render () {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <SignUpView />
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+    flex: 1
+  }
+})
 
-AppRegistry.registerComponent('PineappleFront', () => PineappleFront);
+AppRegistry.registerComponent('PineappleFront', () => PineappleFront)
---
+++ views/splash/index.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  Button,
+  TouchableHighlight
+} from 'react-native';
+
+const background = require("./pineappple.png");
+
+
+export default class InitialScreen extends Component {
+  render () {
+  return(
+
+    <View style={styles.container}>
+      <Image
+        source={background}
+        style={[styles.container, styles.bg]}
+        resizeMode="cover"
+        />
+      <TouchableOpacity
+        style={styles.button}>
+        <Text style={styles.buttonText}>BAND</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}>
+        <Text style={styles.buttonText}>MUSICIAN</Text>
+      </TouchableOpacity>
+    </View>
+  )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  bg: {
+    paddingTop: 30,
+    width: null,
+    height: null
+  },
+  band: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 40
+  },
+  button: {
+    flex: 0.3,
+    borderWidth: 1,
+    height: 50,
+    borderColor: 'blue',
+    borderRadius: 3,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  buttonText: {
+    fontSize: 24,
+    fontWeight: 'bold'
+  }
+})
