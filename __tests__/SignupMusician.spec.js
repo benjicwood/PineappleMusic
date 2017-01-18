037022f43902c4a25fc86fb99de58fb0478ff665
Ulisses85 <lukasz.kuligowski@gmail.com>
2017-01-18 12:45:45 +0000

    [feat-107]- review minor amendments

--- __tests__/SignupMusician.spec.js
+++ __tests__/SignupMusician.spec.js
@@ -2,7 +2,7 @@
 import 'react-native';
 import React from 'react';
 // import PineappleFront from '../index.android.js';
-import SignupMusician from '../views/signupMusician/index.js';
+import SignupMusician from '../App/views/signupMusician/index.js';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
