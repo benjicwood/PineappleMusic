2f24cfd6b34bfb6f5050bae35a0e8cb12dd56e65
benjicwood <bencharlwood@gmail.com>
2017-01-19 17:22:11 +0000

    linting perfect

--- App/reducers/matches.js
+++ App/reducers/matches.js
@@ -1,35 +1,29 @@
 import * as types from '../actions/types';
 
 const initState = {
-    isLoading: false,
-    error: '',
-    userMatches: [],
+  isLoading: false,
+  error: '',
+  userMatches: []
 };
 
-export default function matches(state , action) {
-
-    state = state || initState;
-
-    switch (action.type) {
-
-        case types.FETCH_MATCHES_REQ:
-            return Object.assign({}, state, {
-                isLoading: true
-            });
-
-        case types.FETCH_MATCHES_SUCCESS:
-            return Object.assign({}, state, {
-                isLoading: false,
-                userMatches: action.data
-            });
-
-        case types.FETCH_MATCHES_ERROR:
-            return Object.assign({}, state, {
-                isLoading: false,
-                error: action.error
-            });
-
-        default:
-            return state;
-    }
-};
+export default function matches (state, action) {
+  state = state || initState;
+  switch (action.type) {
+    case types.FETCH_MATCHES_REQ:
+      return Object.assign({}, state, {
+        isLoading: true
+      });
+    case types.FETCH_MATCHES_SUCCESS:
+      return Object.assign({}, state, {
+        isLoading: false,
+        userMatches: action.data
+      });
+    case types.FETCH_MATCHES_ERROR:
+      return Object.assign({}, state, {
+        isLoading: false,
+        error: action.error
+      });
+    default:
+      return state;
+  }
+}
