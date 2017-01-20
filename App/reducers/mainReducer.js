df46b3f211a03c4dbaabc7d99837dedde1f38d90
Hoofman <hoofman@sdf-eu.org>
2017-01-20 12:47:11 +0000

    mainReducer refactor, remove old reducers & reducer tests

---
+++ App/reducers/mainReducer.js
@@ -0,0 +1,15 @@
+import { combineReducers } from 'redux';
+import profile from './profile';
+import genre from './genre';
+import instrument from './instrument';
+import matches from './matches';
+import myHeaven from './myHeaven';
+import myHell from './myHell';
+import theirHeaven from './theirHeaven';
+
+
+const mainReducer = combineReducers({
+  profile, connection, genre, instrument, matches, myHeaven, myHell, theirHeaven
+});
+
+export default mainReducer;
