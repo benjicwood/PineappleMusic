a873b6cc09e323b5fc8df702539b9d330dd7b47b
Ulisses85 <lukasz.kuligowski@gmail.com>
2017-01-23 12:12:47 +0000

    adding youtube playlist in Webview

--- App/views/profile/stream.js
+++ App/views/profile/stream.js
@@ -16,10 +16,21 @@ export default class RCTYouTubeExample extends Component {
 render() {
   return (
 <WebView
- source={{uri: 'https://www.youtube.com/watch?v=5UNDENi3vHM'}}
- style={{marginTop: 20}}
+        style={{flex:1}}
+        javaScriptEnabled={true}
+        source={{uri: 'https://www.youtube.com/playlist?list=PL1F461DA85438E137'}}
 />
+
   )
 }
 
 }
+
+const styles = StyleSheet.create({
+  container: {
+  height:240,
+  width: 350,
+  justifyContent:'center',
+  alignItems:'center',
+  backgroundColor:'black'}
+});
